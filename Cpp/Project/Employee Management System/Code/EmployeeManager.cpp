ad0bd59b1925f4048bd1ad9d3b5ec54171ad7c40
Yifei Zhang <y3698zha@uwaterloo.ca>
2022-09-07 11:19:56 -0400

    program

--- Cpp/Project/Employee Management System/Code/EmployeeManager.cpp
+++ Cpp/Project/Employee Management System/Code/EmployeeManager.cpp
@@ -1,8 +1,25 @@
-#include "EmployeeManager.hpp"
+#include "EmployeeManager.h"
 
 // constructor definition
 EmpolyeeManager::EmpolyeeManager()
 {
+}
+
+// show menu
+void EmpolyeeManager::showMenu()
+{
+    cout << "***************************************************" << endl;
+    cout << "----   Welcome to Employee Management System   ----" << endl;
+    cout << "**************  0. Exit the System       **********" << endl;
+    cout << "**************  1. Add New Employee      **********" << endl;
+    cout << "**************  2. Employee Information  **********" << endl;
+    cout << "**************  3. Delete Employee       **********" << endl;
+    cout << "**************  4. Motify Employee       **********" << endl;
+    cout << "**************  5. Find Employee         **********" << endl;
+    cout << "**************  6. Sort the Employee     **********" << endl;
+    cout << "**************  7. Clear the System      **********" << endl;
+    cout << "***************************************************" << endl;
+    cout << endl;
 }
 
 // destructor definition
---
+++ Cpp/Project/Employee Management System/Code/Employee_Management_System.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "EmployeeManager.h"
+using namespace std;
+
+int main(){
+
+    //init manager object
+    EmpolyeeManager em;
+
+    em.showMenu();
+
+    return 0;
+}
