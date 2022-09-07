2d0598d992bf851482d36cdb10dc377fec19680d
Yifei Zhang <y3698zha@uwaterloo.ca>
2022-09-06 21:30:49 -0400

    new project

---
+++ Cpp/Project/Employee Management System/Code/EmployeeManager.cpp
@@ -0,0 +1,11 @@
+#include "EmployeeManager.hpp"
+
+// constructor definition
+EmpolyeeManager::EmpolyeeManager()
+{
+}
+
+// destructor definition
+EmpolyeeManager::~EmpolyeeManager()
+{
+}
---
+++ Cpp/Project/Employee Management System/Code/EmployeeManager.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class EmpolyeeManager{
+
+public:
+    //constructor declaration
+    EmpolyeeManager();
+
+    //show menu
+    void showMenu();
+
+    //destructor declaration
+    ~EmpolyeeManager();
+};
