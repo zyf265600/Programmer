997229889dc7b219b96389dd30ae36317918643b
zyf265600 <zhangyifeide@gmail.com>
2023-09-20 16:25:32 -0400

    maven

---
+++ Maven/Code_Material/itheima_maven/HelloWorld/src/test/java/HelloWorldTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+public class HelloWorldTest {
+   @Test
+    public void test1(){
+       System.out.println("Hello World Test 1!");
+   }
+
+    @Test
+    public void test2(){
+        System.out.println("Hello World Test 2!");
+    }
+
+    @Test
+    public void test3(){
+        System.out.println("Hello World Test 3!");
+    }
+
+}
