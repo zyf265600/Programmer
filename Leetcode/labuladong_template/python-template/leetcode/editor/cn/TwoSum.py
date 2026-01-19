0862dbe4e138c8828438f91ec9f0bde2a05fb0e0
zyf265600 <zhangyifeide@gmail.com>
2026-01-19 10:46:38 -0500

    learning R

--- Leetcode/labuladong_template/python-template/leetcode/editor/cn/TwoSum.py
+++ Leetcode/labuladong_template/python-template/leetcode/editor/cn/TwoSum.py
@@ -7,7 +7,7 @@ from leetcode.editor.common.node import *
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         bank = {}
-        
+
         for i, n in enumerate(nums):
             rest = target - n
             if rest in bank:
